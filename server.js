64c52baf5e26811b270341d7dfd3891d3b398e3f
Glitch (hello-express) <none>
2020-12-16 20:15:42 +0000

    🏨🤑 Checkpoint ./CODE_OF_CONDUCT.md:5900777/6778 ./LICENSE.md:5900777/1069 ./server.js:5900777/1745

--- server.js
+++ server.js
@@ -32,3 +32,65 @@ app.get("/dreams", (request, response) => {
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+const {Client, RichEmbed } = require('discord.js')
+
+const Discord = require("discord.js");
+ 
+const client = new Client()
+ 
+const PREFIX = '!'
+
+
+
+client.on('ready',() => {    
+    let myGuild = client.guilds.get('689139264960856101');
+    let memberCount = myGuild.memberCount;
+    client.user.setActivity(memberCount + ` אנשים בשרת `, {type: "WATCHING"})
+    console.log(`${client.user.tag} is online`)
+})
+
+
+client.on('guildMemberAdd', member => {
+  let bicon = member.user.displayAvatarURL;
+  let myGuild = client.guilds.get('689139264960856101')
+  let membercount = myGuild.memberCount;
+  const channel = member.guild.channels.find(ch => ch.id === '788543692411109416');
+    const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(bicon)
+      .setDescription(`ברוכים הבאים לשרת ${member}, אתה משתמש מספר, ${membercount} `);
+    channel.send(embed);
+  }
+)
+
+
+client.on('message', message => {
+  let args = message.content.substring(PREFIX.length).split(" ");
+  switch(args[0]){
+      case 'verify':
+  const embed = new Discord.RichEmbed()
+  .setFooter('Dev By RoTeX_YT#5719')
+  .addField('**בשביל לראות את כול החדרים תלחצו על האימוגי**',` ✅`)
+  .setColor("RANDOM")
+
+.message.channel.send(embed);
+message.delete()
+}}
+);
+ 
+
+client.on('guildMemberAdd', member => {
+        let bicon = member.user.displayAvatarURL;
+        const channel = member.guild.channels.find(ch => ch.id === '689139264960856101');
+          const embed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .setThumbnail(bicon)
+            .setDescription(`**Welcome to GOV-RP | Auditions**`);
+          member.send(embed);
+        }
+      )
+
+
+client.login('Nzg4NTUxNTg0NDE3NDQ3OTY3.X9lJ5A.Xt5qGOSBiCb2iVGD00aBehSp-K4');//token
