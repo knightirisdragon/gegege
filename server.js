518602bc63d0288cc93725540fe5825c77eba14a
Glitch (bedecked-sphenoid-ceratonykus) <none>
2020-12-17 23:25:52 +0000

    🎒📊 Checkpoint ./server.js:5900777/2443:7647955/5314

--- server.js
+++ server.js
@@ -1,52 +1,56 @@
 const express = require("express");
 const app = express();
-const {Client, RichEmbed } = require('discord.js')
+const { Client, RichEmbed } = require("discord.js");
 const Discord = require("discord.js");
-const client = new Client()
+const client = new Client();
 const { MessageEmbed } = require("discord.js");
-const PREFIX = '!'
+const PREFIX = "!";
+
+client.on("ready", () => {
+  let myGuild = client.guilds.cache.get("689139264960856101");
+  let memberCount = myGuild.memberCount;
+  client.user.setActivity(memberCount + ` אנשים בשרת `, { type: "WATCHING" });
+  console.log(`${client.user.tag} is online`);
+});
 
 
-
-client.on('ready',() => {    
-    let myGuild = client.guilds.cache.get('689139264960856101');
-    let memberCount = myGuild.memberCount;
-    client.user.setActivity(memberCount + ` אנשים בשרת `, {type: "WATCHING"})
-    console.log(`${client.user.tag} is online`)
-})
-
-
-client.on('guildMemberAdd', member => {
+client.on("guildMemberAdd", member => {
   let bicon = member.user.displayAvatarURL;
-  let myGuild = client.guilds.cache.get('689139264960856101')
+  let myGuild = client.guilds.cache.get("689139264960856101");
   let membercount = myGuild.memberCount;
-  const channel = member.guild.channels.cache.find(ch => ch.id === '788543692411109416');
-    const embed = new Discord.MessageEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(bicon)
-      .setDescription(`ברוכים הבאים לשרת ${member}, אתה משתמש מספר, ${membercount} `);
-    channel.send(embed);
-  }
-)
+  const channel = member.guild.channels.cache.find(
+    ch => ch.id === "788543692411109416"
+  );
+  const embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setThumbnail(member.user.avatarURL())
+    .setDescription(
+      `ברוכים הבאים לשרת ${member}, אתה משתמש מספר, ${membercount} `
+    );
+  channel.send(embed);
+});
 
-client.on('message', message => {
+
+
+client.on("message", message => {
   let args = message.content.substring(PREFIX.length).split(" ");
-  switch(args[0]){
-      case 'verify':
-  const embed = new Discord.RichEmbed()
-  .setFooter('Dev By RoTeX_YT#5719')
-  .addField('**בשביל לראות את כול החדרים תלחצו על האימוגי**',` ✅`)
-  .setColor("RANDOM")
+  switch (args[0]) {
+    case "verify":
+      const embed = new Discord.RichEmbed()
+        .setFooter("Dev By RoTeX_YT#5719")
+        .addField("**בשביל לראות את כול החדרים תלחצו על האימוגי**", ` ✅`)
+        .setColor("RANDOM")
 
-.message.channel.send(embed);
-message.delete()
-}}
-);
- 
+        .message.channel.send(embed);
+      message.delete();
+  }
+});
+
+
 
 client.on('guildMemberAdd', member => {
         let bicon = member.user.displayAvatarURL;
-        const channel = member.guild.channels.cache.find(ch => ch.id === '689139264960856101');
+        const channel = member.guild.channels.find(ch => ch.id === '689139264960856101');
           const embed = new Discord.RichEmbed()
             .setColor("RANDOM")
             .setThumbnail(bicon)
@@ -55,5 +59,4 @@ client.on('guildMemberAdd', member => {
         }
       )
 
-
-client.login('Nzg4NTUxNTg0NDE3NDQ3OTY3.X9lJ5A.Xt5qGOSBiCb2iVGD00aBehSp-K4');//token
+client.login("Nzg4NTUxNTg0NDE3NDQ3OTY3.X9lJ5A.Xt5qGOSBiCb2iVGD00aBehSp-K4"); //token
